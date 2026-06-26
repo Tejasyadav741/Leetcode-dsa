6548edefc6e77847d6e743a2b68cde93847b2d8d
Tejasyadav741 <tejasyadav1852007@gmail.com>
2026-06-26 20:01:56 +0530

    Time: 0 ms (100%), Space: 43.1 MB (95.13%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -3,7 +3,7 @@ class Solution {
         for(int i= digits.length-1;i>=0;i--)
         {
             if(digits[i]<9){
-            digits[i]++;
+            digits[i]+=1;
             return digits;
             }
             else
